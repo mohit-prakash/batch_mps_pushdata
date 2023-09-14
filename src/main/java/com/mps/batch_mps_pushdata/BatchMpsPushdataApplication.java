8977c8c384ce646e5bc6028e1dd1628d00bc87b6
Mohit Prakash <m2450p@gmail.com>
2023-09-14 14:11:52 +0530

    First Commit

---
+++ src/main/java/com/mps/batch_mps_pushdata/BatchMpsPushdataApplication.java
@@ -0,0 +1,13 @@
+package com.mps.batch_mps_pushdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchMpsPushdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchMpsPushdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mps/batch_mps_pushdata/BatchMpsPushdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mps.batch_mps_pushdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchMpsPushdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
